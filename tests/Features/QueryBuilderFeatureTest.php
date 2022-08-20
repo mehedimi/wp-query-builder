ebcb2d126acf20d67c8fa6ef90fb3c2370b4b7c3
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2022-08-20 10:43:28 +0600

    Delete phpunit.xml

---
+++ tests/Features/QueryBuilderFeatureTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Mehedi\WPQueryBuilderTests\Features;
+
+use Mehedi\WPQueryBuilder\Query\Grammar;
+use PHPUnit\Framework\TestCase;
+
+abstract class QueryBuilderFeatureTest extends TestCase
+{
+}
